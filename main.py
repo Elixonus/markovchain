1e5a67324a20f6a847b8b1aeafaa7a12f40bdef4
Elias Dobrin <elias@dobrin.net>
2022-06-10 16:38:10 -0700

    combined into just one table

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from rich.console import Console
 from rich.table import Table
-from markov import Markov, Node, Link
+from markov import Markov
 
-markov = Markov.from_string("a b c b b b a")
-print(markov.nodes[1].links)
-
+markov = Markov.from_string("cloudy sunny cloudy rainy cloudy sunny cloudy cloudy")
 console = Console()
-
-table = Table(title="Markov Chain Nodes")
-table.add_column("Node Name")
-table.add_column("Node ID")
-for node in markov.get_nodes():
-    table.add_row(f"{node.name}", f"N{id(node)}")
-console.print(table)
-
-table = Table(title="Markov Chain Links")
-table.add_column("Link Relationship")
-table.add_column("Link Probability")
-table.add_column("Link ID")
+table = Table(title="Markov Chain Events")
+table.add_column("Relationship")
+table.add_column("Probability")
 for link in markov.get_links():
-    table.add_row(f"{link.from_node.name} -> {link.node.name}", f"{link.weight:.3f}", f"L{id(link)}")
+    table.add_row(f"{link.from_node.name} -> {link.node.name}", f"{link.weight:.3f}")
 console.print(table)
