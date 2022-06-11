e22b9458e9a12abba5d513befe249b7c2d49aaab
Elias Dobrin <elias@dobrin.net>
2022-06-11 15:53:58 -0700

    made dynamic text based on user input

--- command_line.py
+++ command_line.py
@@ -2,7 +2,22 @@ from rich.console import Console
 from rich.table import Table
 from markov import Markov
 
-markov = Markov.from_string("cloudy sunny cloudy rainy cloudy sunny cloudy cloudy")
+markov: Markov
+
+while True:
+    try:
+        message = input("Input: ")
+        split = message.split(" ")
+        assert len(split) >= 2
+        for s in split:
+            assert len(s) >= 1
+        markov = Markov.from_string(message)
+        break
+    except:
+        print("Error try again.")
+        continue
+
+print()
 console = Console()
 table = Table(title="Markov Chain Events")
 table.add_column("Relationship")
@@ -10,3 +25,5 @@ table.add_column("Probability")
 for link in markov.get_links():
     table.add_row(f"{link.from_node.name} -> {link.node.name}", f"{link.weight:.3f}")
 console.print(table)
+
+input()
